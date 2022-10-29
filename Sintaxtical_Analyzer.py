f02ab588c72fc9ebc504d144f0e379d7f89f2cd8
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-29 11:30:08 -0300

    fix tokens comments, asign and more

--- Sintaxtical_Analyzer.py
+++ Sintaxtical_Analyzer.py
@@ -1,4 +1,13 @@
 import ply.lex as lex 
+# Import Data
+filename='textoPrueba.txt'
+try:
+    f = open(filename)
+    data = f.read()
+    f.close()   
+except IndexError:
+    print('Error')
+    data=''
 
 reserved = {
   'PROG_BEGIN' : 'PROG_BEGIN',
@@ -32,58 +41,28 @@ reserved = {
   'STOP' : 'STOP',
   }
 
-tokens = [
-  'FIN_LINEA',
-  'PICO_OPEN',
-  'PICO_CLOSE',
-  'PARENT_OPEN',
-  'PARENT_CLOSE',
-
-  'EXTENSION',
-  'COMA',
-
-  'VALOR_ENTERO',
-  'VALOR_TEXTO',
-  'DECIMAL',
-  'LOGICO',
-
-  'DPUNTOS',
-  
-  'ASIGNAR',
-  'OPERADOR',  
-
-  'TIPO_PIN',
-
-  'NOMBRE_VAR'
-] + list(reserved.values())
-
+tokens = ['PUNTO','PICO_OPEN','PICO_CLOSE','PARENT_OPEN','PARENT_CLOSE','COMA','VALOR_ENTERO','VALOR_TEXTO','DECIMAL','LOGICO','DPUNTOS','ASIGNAR','OPERADOR','TIPO_PIN','NOMBRE_VAR'] + list(reserved.values())
 
 #Exp. regulares
-t_PUNTO = r'\.'
-
+t_PUNTO = r'\.' #change name more general
 t_PICO_OPEN = r'<'
 t_PICO_CLOSE = r'>'
 t_PARENT_OPEN = r'\('
 t_PARENT_CLOSE = r'\)'
-
-t_EXTENSION = r'\.[a-zA-Z]+'
 t_COMA = r'\,'
-
 t_VALOR_ENTERO = r'[0-9]+'
 t_VALOR_TEXTO = r'\".*\"'
 t_DECIMAL = r'[0-9]+\.[0-9]+'
-
-t_DPUNTOS = r'\:'
 t_ASIGNAR = r'(:=)'
-t_OPERADOR = r'(\+ | - | \* | \/ | >= | <= | == | !=)'
-
-#3. Definimos una funcion para ignorar los espacios
+t_DPUNTOS = r'\:'
+t_OPERADOR = r'(\+ | - | >= | <= | == | !=)' # disable / signs
+#3. Ignores
 t_ignore_ESPACIOS = r'[ ]+'
-t_ignore_COMENT = r'(\/\/.*) | (\/\*.*\*\/) ' 
+t_ignore_COMENT =  r'(/\*(.|\n)*?\*/)|(//.*)' #fix
 
 def t_NOMBRE_VAR(t):
   r'[a-zA-Z][a-zA-Z0-9_]+'
-  t.type = reserved.get(t.value,'NOMBRE_VAR')    # Check for reserved words
+  t.type = reserved.get(t.value,'NOMBRE_VAR') # Check for reserved words
   return t
 
 def t_error(t):
@@ -94,16 +73,13 @@ def t_newline(t):
   r'\n'
   t.lexer.lineno+=1
 
-#4. Guardamos el archivo y generamos el compilador
-__file__ = "Avance 1 Proyecto Compiladores.ipynb"
+#DEF LEX
 lexer = lex.lex()
-
-#5. Probamos el analizador lexico pasandole una cadena
-data = input("Ingrese una cadena: ")
 lexer.input(data)
 
-print('Token - Lexema')
+print('Token - Lexema - Line')
 while True:
   tok = lexer.token()
   if not tok: break
-  print('(', tok.type, ',', tok.value, ')')
+  print('(', tok.type, ',', tok.value, ',', tok.lineno, ')')
+
